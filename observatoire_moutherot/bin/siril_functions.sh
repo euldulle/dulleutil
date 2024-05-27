410b6ac40707cdc7c368f69eace81237c508b784
euldulle <fm@dulle.fr>
2024-05-27 15:08:57 +0000

    slow progress but still...

--- observatoire_moutherot/bin/siril_functions.sh
+++ observatoire_moutherot/bin/siril_functions.sh
@@ -6,8 +6,29 @@ export SIRCALIB="$SIRROOT/Poseidon_ccd"
 export SIRTMP="$SIRROOT/tmp"
 export SIRLOG="$SIRROOT/log"
 export PATH="$SIRBIN:$PATH"
+source ~/git/dulleutil/observatoire_moutherot/bin/siril-completion
 
-chkseq(){
+srl_guesstype(){
+    shopt -s nocasematch;
+    if [[ ${1} =~ ^(bias|biases|offset) ]]; then
+        echo "Bias"
+        return 0
+    fi
+
+    if [[ ${1} =~ ^(dark) ]]; then
+        echo "Dark"
+        return 0
+    fi
+
+    if [[ ${1} =~ ^(flat) ]]; then
+        echo "Flat"
+        return 0
+    fi
+    echo "Light"
+    return 1
+}
+
+srl_chkseq(){
     let count=0
     regex=""
     seqs="all"
@@ -65,6 +86,38 @@ if ! test "$?" = "0"; then
 fi
 }
 export chkseq
+
+srl_mkmaster(){
+    if test -z "$rootdir"; then
+        rootdir=$(pwd)
+    fi
+
+    seqname="$1"
+    IFS=_ read cname stamp <<<$(echo $seqname) # separates canonical name from whatever lies after the first '_'
+    mastername="$2"
+    if test -z "$mastername"; then # no master name provided
+        IFS=- read typ filter exposure bin dum <<<$(echo $cname)
+        imgtyp=$(srl_guesstype $typ )
+        echo "$typ @ $filter @ imgtyp @$imgtyp@"
+        case "$imgtyp" in
+            "Bias")
+                mastername="MasterBias-$bin";;
+            "Dark")
+                mastername="MasterDark-$exposure-$bin";;
+            "Flat")
+                mastername="MasterFlat-$filter-$bin";;
+            *)
+                echo "Unkown frame type @$imgtyp@, not stacking"
+                return 1;;
+        esac
+    fi
+
+    siril -s - <<ENDMKMASTER
+        cd $rootdir
+        stack $seqname rej 3 3 -norm=mul -out=$mastername
+ENDMKMASTER
+    return 0
+}
 
 mkflat(){
     sirilscript=$SIRTMP/flat.ssc
