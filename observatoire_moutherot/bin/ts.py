e3513febb8f945e8dae152468776ffbc9a97f7d4
euldulle <ed@dulle.fr>
2024-09-06 17:23:36 +0000

    threaded version (main + udp + getch)

--- observatoire_moutherot/bin/ts.py
+++ observatoire_moutherot/bin/ts.py
@@ -4,9 +4,20 @@ from time import sleep
 import RPi.GPIO as GPIO
 import curses
 import socket
+import threading
+
 from math import ceil
 #from mx import DateTime
 from gpio_filter_assignments import *
+
+keycode=None
+shared_final = None
+udpsocket=None
+logmsg=None
+OUTWARDS=1
+INWARDS=-1
+backlash=None
+lock = threading.Lock()
 
 def sign(x):
     return (x > 0) - (x < 0)
@@ -20,98 +31,24 @@ def init_listen_udp(port):
     return udp_socket
 
 def udp_update_pos():
-    message, addr = udpsocket.recvfrom(1024)  # Buffer size is 32 bytes
-    #errprint("UDP:",message.decode())
-    final,large,small,status=message.decode().split()
-    return float(final),float(large),float(small),int(status)
+    global shared_final 
+    udpsocket=init_listen_udp(2345)
+    while True:
+        message, addr = udpsocket.recvfrom(32)  # Buffer size is 32 bytes
+        final,large,small,status=message.decode().split()
 
-stdscr = curses.initscr()
-try:
-    stdscr.curs_set(False)
-    curses.curs_set(False)
-except:
-    pass
-curses.noecho()
-s="    STEP TAK FOCUS    "
-stdscr.addstr(0,0,s,curses.A_REVERSE)
-s="  POS(um)  INC (um) "
-stdscr.addstr(1,1,s,curses.A_BOLD)
-
-step_scale=[1,2,5,10,20,50,100,200,500,1000,2000,5000,10000]
-step_dir=-1
-max_range=12
-step_range=0
-step_inc=1
-old_dir=1
-step_pos=0
-ustep_count=0
-steps_per_um=0.0104
-usteps_per_step=32
-delay_step=0.01/usteps_per_step
-usteps_per_um=0.500
-#
-#
-# GPIO pin assingnment
-#
-#o_step_c14=26
-#o_dir_c14=19
-#o_enable_c14=13
-
-GPIO.setmode(GPIO.BCM)
-
-#
-#
-# GPIO pin dir settings
-#
-GPIO.setup(o_step_c14, GPIO.OUT)
-GPIO.setup(o_dir_c14, GPIO.OUT)
-GPIO.setup(o_enable_c14, GPIO.OUT)
-#
-# GPIO pin init
-#
-GPIO.output(o_step_c14,GPIO.HIGH)
-GPIO.output(o_step_c14,GPIO.LOW)
-GPIO.output(o_dir_c14,GPIO.HIGH)
-GPIO.output(o_dir_c14,GPIO.LOW)
-GPIO.output(o_enable_c14,GPIO.HIGH)
-
-#
-#
-# saving term settings
-#
-fd = sys.stdin.fileno()
-initial_settings = termios.tcgetattr(fd)
-
-#
-#
-# Creating getch call
-#
-try:
-    from msvcrt import getch  # try to import Windows version
-except ImportError:
-    def getch():   # define non-Windows version
-        fd = sys.stdin.fileno()
-#        try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-        return ch
-#       finally:
-#           termios.tcsetattr(fd, termios.TCSADRAIN, initial_settings)
-#       return ch
-
-#
-# Init No key press
-#
-keycode = None
+        with lock:
+            shared_final=float(final)
 
 #
 # Threaded kb lookup
 #
 def keypress():
     global keycode
-    keycode = ord(getch())
+    while True:
+        keycode = ord(getch())
+        sleep(.01)
 
-_thread.start_new_thread(keypress, ())
 
 def get_usteps_from_dist(dist,rate):
     # dist is in mm
@@ -120,34 +57,38 @@ def get_usteps_from_dist(dist,rate):
     # return value is in usteps
     return abs(int(ceil(dist)*rate))
 
-def goto(target, initial, usteps, direction): # direction = INWARDS (-1) or OUTWARDS (+1)
-    maxmove=40
+def goto(target, usteps, direction): # direction = INWARDS (-1) or OUTWARDS (+1)
+    global backlash, shared_final, usteps_per_um
+    maxmove=60
     if usteps==0:
         return
-    initial,large,small,status=udp_update_pos()
-    new=initial
+    # initial,large,small,status=udp_update_pos()
     count=0
-    udpreads=1
     try:
         usteps=backlash*(direction!=goto.direction)+usteps
     except:
         goto.direction=direction
-    errprint("Goto : target %.3f initial %.3f"%(target,initial))
-    while(abs(target-new)>0.005 and count<maxmove):
+    with lock:
+        current=shared_final
+        
+    errprint("Goto : target %.3f current %.3f"%(target,shared_final))
+    while(abs(target-current)>0.005 and count<maxmove):
         count+=1
-        errprint("DoMove : %d steps dir %d (target %.3f new %.3f"%(usteps,direction,target,new))
+        errprint("DoMove : %d steps dir %d (target %.3f current %.3f)"%(usteps,direction,target,current))
+        usteps=backlash*(direction!=goto.direction)+max(4,get_usteps_from_dist(1000*(current-target),usteps_per_um))
         do_move(usteps,direction)
-        for i in range(udpreads):
-            new,large,small,status=udp_update_pos()
-        new,large,small,status=udp_update_pos()
         goto.direction=direction
-        direction=sign(target-new)
-        usteps=backlash*(direction!=goto.direction)+max(4,get_usteps_from_dist(1000*(new-target),usteps_per_um))
-        udpreads=1
-
+        sleep(.2)
+        with lock:
+            current=shared_final
+        direction=sign(target-current)
+        usteps=backlash*(direction!=goto.direction)+max(4,get_usteps_from_dist(1000*(current-target),usteps_per_um))
+    with lock:
+        final=shared_final
     if(count>=maxmove):
-        errprint("Max reached : target %.3f final %.3f delta %.3f \n"%(target,new,new-target))
-    errprint("target %.3f final %.3f delta %.3f \n"%(target,new,new-target))
+        errprint("Max reached Goto : target %.3f final %.3f delta %.3f \n"%(target,final,final-target))
+    else:
+        errprint("Goto OK: target %.3f final %.3f delta %.3f \n"%(target,final,final-target))
 
 def do_move(usteps,direction):
     global step_scale, step_range, delay_step, step_pos, old_dir, ustep_count, step_dir, udpsocket,logmsg
@@ -172,68 +113,168 @@ def do_move(usteps,direction):
     GPIO.output(o_enable_c14,GPIO.HIGH)
     GPIO.output(o_enable_c14,GPIO.LOW)
 
-global udpsocket, logmsg, OUTWARDS, INWARDS, backlash
-OUTWARDS=1
-INWARDS=-1
-backlash=200 # unit is ustep
+def process_data():
+    global shared_final, keycode
+    global backlash, usteps_per_um, delay_step
+    backlash=50 # unit is ustep
 
-udpsocket=init_listen_udp(2345)
-final=0
-pwr_stepper(ts_drv_addr, ON)
-logmsg="ready"
-#
-#
-#
-# n=DateTime.now()
-target=0
-while True:
-    delta_dist=float(step_scale[step_range]) # requested delta_dist in um
-    delta_usteps=get_usteps_from_dist(delta_dist,usteps_per_um) # corresponding nr of usteps
-    current,large,small,status=udp_update_pos()
-    if keycode==27:
-        keycode=ord(getch())
-        if keycode==91:
-            keycode=ord(getch())
-            if keycode==65:   # up
-                step_range=min(max_range,step_range+1)
-            elif keycode==66: # down
-                step_range=max(0,step_range-1)
-            elif keycode==68: # left inwards
-                target=current-delta_dist/1000
-                goto(target,current,delta_usteps, INWARDS)
-            elif keycode==67: # left
-                target=current+delta_dist/1000 # target in mm
-                goto(target,current,delta_usteps, OUTWARDS)
-                #logmsg="T%.3f"%target
-    elif keycode==122:
-        step_pos=0
-        ustep_count=0
-    elif keycode==3:
-        termios.tcsetattr(fd, termios.TCSADRAIN, initial_settings)
-        curses.nocbreak();
-        stdscr.keypad(0);
-        curses.echo()
-        curses.endwin()
-        GPIO.cleanup()
-        pwr_stepper(ts_drv_addr, OFF)
-        sys.exit()
-        break
-
-    if keycode is not None:
-        #print keycode
-        keycode=None
-        _thread.start_new_thread(keypress, ())
-    #s="\n %+6d  %7.1f\n\n    %s\n"%(step_pos*steps_per_um, steps_per_um*step_scale[step_range],n.strftime("%H:%M:%S"))
-    #s="\n %+6d  %7.1f\n\n  usteps : %d  %s\n"%(step_pos, step_scale[step_range], ustep_count, "ready")
-    s="\n %+7.3f  %7.1f\n\n  udp : %s  %s\n"%(current, step_scale[step_range], final, logmsg)
+    #udpsocket=init_listen_udp(2345)
+    logmsg="ready"
+    stdscr = curses.initscr()
     try:
-        stdscr.addstr(2,1,s,curses.A_BOLD)
+        stdscr.curs_set(False)
+        curses.curs_set(False)
     except:
-        #print ("%s Erreur (fenetre trop petite ?)\r"%(n.strftime("%H:%M:%S")));
-        print ("%s Erreur (fenetre trop petite ?)\r"%("bin non"));
-    stdscr.refresh()
-#    n=DateTime.now()
-    sys.stderr.flush()
-    time.sleep(0.01)
+        pass
+    curses.noecho()
+    s="    STEP TAK FOCUS    "
+    stdscr.addstr(0,0,s,curses.A_REVERSE)
+    s="  POS(um)  INC (um) "
+    stdscr.addstr(1,1,s,curses.A_BOLD)
 
-pwr_stepper(ts_drv_addr, OFF)
+    step_scale=[1,2,5,10,20,50,100,200,500,1000,2000,5000,10000]
+    step_dir=-1
+    max_range=12
+    step_range=0
+    step_inc=1
+    old_dir=1
+    step_pos=0
+    ustep_count=0
+    steps_per_um=0.0104
+    usteps_per_step=32
+    delay_step=0.01/usteps_per_step
+    usteps_per_um=0.200
+    #
+    #
+    # GPIO pin assingnment
+    #
+    #o_step_c14=26
+    #o_dir_c14=19
+    #o_enable_c14=13
+
+    GPIO.setmode(GPIO.BCM)
+
+    #
+    #
+    # GPIO pin dir settings
+    #
+    GPIO.setup(o_step_c14, GPIO.OUT)
+    GPIO.setup(o_dir_c14, GPIO.OUT)
+    GPIO.setup(o_enable_c14, GPIO.OUT)
+    #
+    # GPIO pin init
+    #
+    GPIO.output(o_step_c14,GPIO.HIGH)
+    GPIO.output(o_step_c14,GPIO.LOW)
+    GPIO.output(o_dir_c14,GPIO.HIGH)
+    GPIO.output(o_dir_c14,GPIO.LOW)
+    GPIO.output(o_enable_c14,GPIO.HIGH)
+
+    #
+    #
+    # saving term settings
+    #
+    fd = sys.stdin.fileno()
+    initial_settings = termios.tcgetattr(fd)
+
+    #
+    # Init No key press
+    #
+    keycode = None
+
+    while True:
+        delta_dist=float(step_scale[step_range]) # requested delta_dist in um
+        delta_usteps=get_usteps_from_dist(delta_dist,usteps_per_um) # corresponding nr of usteps
+        # current,large,small,status=udp_update_pos()
+        with lock:
+            current=shared_final
+        if keycode==27:
+            keycode=ord(getch())
+            if keycode==91:
+                keycode=ord(getch())
+                if keycode==65:   # up
+                    step_range=min(max_range,step_range+1)
+                elif keycode==66: # down
+                    step_range=max(0,step_range-1)
+                elif keycode==68: # left inwards
+                    target=current-delta_dist/1000
+                    goto(target,delta_usteps, INWARDS)
+                elif keycode==67: # left
+                    target=current+delta_dist/1000 # target in mm
+                    goto(target,delta_usteps, OUTWARDS)
+                    #logmsg="T%.3f"%target
+        elif keycode==122:
+            step_pos=0
+            ustep_count=0
+        elif keycode==3:
+            termios.tcsetattr(fd, termios.TCSADRAIN, initial_settings)
+            curses.nocbreak();
+            stdscr.keypad(0);
+            curses.echo()
+            curses.endwin()
+            GPIO.cleanup()
+            #pwr_stepper(ts_drv_addr, OFF)
+            sys.exit()
+            break
+
+#        if keycode is not None:
+            #print keycode
+#            keycode=None
+            #_thread.start_new_thread(keypress, ())
+        if shared_final is not None:
+            s="\n %+7.3f (%7.3f)  %7.1f\n\n  udp : %s\n"%(current, shared_final, step_scale[step_range], logmsg)
+
+        try:
+            stdscr.addstr(2,1,s,curses.A_BOLD)
+        except:
+            #print ("%s Erreur (fenetre trop petite ?)\r"%(n.strftime("%H:%M:%S")));
+            print ("%s Erreur (fenetre trop petite ?)\r"%("bin non"));
+        stdscr.refresh()
+    #    n=DateTime.now()
+        sys.stderr.flush()
+        time.sleep(0.01)
+
+# Main function
+if __name__ == "__main__":
+    #
+    #
+    # Creating getch call
+    #
+    try:
+        from msvcrt import getch  # try to import Windows version
+    except ImportError:
+        def getch():   # define non-Windows version
+            fd = sys.stdin.fileno()
+    #        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+            return ch
+    #       finally:
+    #           termios.tcsetattr(fd, termios.TCSADRAIN, initial_settings)
+    #       return ch
+
+    #pwr_stepper(ts_drv_addr, ON)
+#
+#
+#
+    # Start the UDP listener thread
+    listener_thread = threading.Thread(target=udp_update_pos)
+    listener_thread.daemon = True
+    listener_thread.start()
+
+    keypress_thread = threading.Thread(target=keypress)
+    keypress_thread.daemon = True
+    keypress_thread.start()
+
+    # Start the data processing thread
+    processor_thread = threading.Thread(target=process_data)
+    processor_thread.daemon = True
+    processor_thread.start()
+
+#    _thread.start_new_thread(keypress, ())
+
+    # Keep the main thread alive
+    while True:
+        time.sleep(1)
+
+    #pwr_stepper(ts_drv_addr, OFF)
